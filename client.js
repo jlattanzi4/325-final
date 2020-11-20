5c1d736f70fe3683435b58f5d70f2cce045e1b3a
17lmerida <17lmerida@users.noreply.github.com>
2020-11-19 20:11:20 -0500

    Added the JAZZ to get alerts looking nice and will ask user to enter email or name or tell them a room is full.

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+"use strict";
 const url = "http://localhost:8080/counter"; // NOTE NEW URL
 
 function counterCreate() {
@@ -69,18 +70,100 @@ function counterDelete() {
 let counterDisplayElem = document.querySelector('.counter-display');
 let counterMinusElem = document.querySelector('.counter-minus');
 let counterPlusElem = document.querySelector('.counter-plus');
-let maxStudents  = document.getElementById('max-students').innerText;
 
-let count = 10;
+let count = 20;
 
 updateDisplay();
 
 counterPlusElem.addEventListener("click",()=>{
+	const maxStudents  = document.getElementById('max-students').innerText;
+	const alert = document.getElementById("alert");
+	const alert2 = document.getElementById("alert2");
+	const name = document.getElementById('exampleFormControlInput1').value;
+	const email = document.getElementById('exampleFormControlInput2').value;
+	
+	if(alert){
+		alert.remove();
+	}
+	if(alert2){
+		alert2.remove();
+	}
+	console.log(count < maxStudents);
 	if(count < maxStudents){
-		count++;
+		if((name && email)){
+			const alert = document.getElementById('namemail');
+			const p = document.createElement('div');
+			const dis = document.createElement('button');
+			p.setAttribute('id','alert')
+			p.classList.add('alert');
+			p.classList.add('alert-success');
+			p.classList.add('alert-dismissable');
+			p.classList.add('fade');
+			p.classList.add('show');
+			p.setAttribute('role', 'alert')
+			p.innerHTML = 'Name success!';
+			dis.setAttribute('type', 'button');
+			dis.classList.add('close');
+			dis.setAttribute('data-dismiss', 'alert');
+			dis.setAttribute('aria-label', 'Close');
+			p.appendChild(dis);
+			alert.appendChild(p);
+			count++;
+		} else {
+			const alert = document.getElementById('namemail');
+			const p = document.createElement('div');
+			const dis = document.createElement('button');
+			p.setAttribute('id','alert')
+			p.classList.add('alert');
+			p.classList.add('alert-danger');
+			p.classList.add('alert-dismissable');
+			p.classList.add('fade');
+			p.classList.add('show');
+			p.setAttribute('role', 'alert')
+			p.innerHTML = 'Please enter name or email';
+			dis.setAttribute('type', 'button');
+			dis.classList.add('close');
+			dis.setAttribute('data-dismiss', 'alert');
+			dis.setAttribute('aria-label', 'Close');
+			p.appendChild(dis);
+			alert.appendChild(p);
+		}
+		const alert = document.getElementById('theForm');
+		const p = document.createElement('div');
+		const dis = document.createElement('button');
+		p.setAttribute('id','alert2')
+		p.classList.add('alert');
+		p.classList.add('alert-success');
+		p.classList.add('alert-dismissable');
+		p.classList.add('fade');
+		p.classList.add('show');
+		p.setAttribute('role', 'alert2')
+		p.innerHTML = 'Great success!';
+		dis.setAttribute('type', 'button');
+		dis.classList.add('close');
+		dis.setAttribute('data-dismiss', 'alert');
+		dis.setAttribute('aria-label', 'Close');
+		p.appendChild(dis);
+		alert.appendChild(p);
 		updateDisplay();
 	} else {
-		count++;
+		const alert = document.getElementById('theForm');
+		const p = document.createElement('div');
+		const dis = document.createElement('button');
+		p.setAttribute('id','alert')
+		p.classList.add('alert');
+		p.classList.add('alert-danger');
+		p.classList.add('alert-dismissable');
+		p.classList.add('fade');
+		p.classList.add('show');
+		p.setAttribute('role', 'alert2')
+		p.innerHTML = 'Sorry, there are too many students currently in this area. Please try again later.';
+		dis.setAttribute('type', 'button');
+		dis.classList.add('close');
+		dis.setAttribute('data-dismiss', 'alert');
+		dis.setAttribute('aria-label', 'Close');
+		p.appendChild(dis);
+		alert.appendChild(p);
 		updateDisplay();
 	}
     
